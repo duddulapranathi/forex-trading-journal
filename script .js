bf35ba38745483c8a4df781686502ecf8863cb9a
duddulapranathi <duddulapranathi@gmail.com>
2025-12-29 23:47:29 +0530

    Add files via upload

---
+++ script .js
@@ -0,0 +1,39 @@
+document.getElementById("tradeForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let entry = Number(entry.value);
+  let exit = Number(document.getElementById("exit").value);
+  let sl = Number(document.getElementById("sl").value);
+  let qty = Number(document.getElementById("qty").value);
+  let type = document.getElementById("type").value;
+
+  let pl = type === "BUY"
+    ? (exit - entry) * qty
+    : (entry - exit) * qty;
+
+  let result = pl > 0 ? "WIN" : "LOSS";
+
+  fetch("YOUR_SCRIPT_URL_HERE", {
+    method: "POST",
+    body: JSON.stringify({
+      date: date.value,
+      pair: pair.value,
+      type,
+      setup: setup.value,
+      timeframe: timeframe.value,
+      entry,
+      sl,
+      target: target.value,
+      qty,
+      exit,
+      pl,
+      result,
+      emotion: emotion.value,
+      notes: notes.value,
+      screenshot: screenshot.value
+    })
+  }).then(() => {
+    alert("Trade Saved ✅");
+    document.getElementById("tradeForm").reset();
+  });
+});
